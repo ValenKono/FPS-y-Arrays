59d740e4725db139c07a73d38b36f942c682c99e
ValenKono <48592024@est.ort.edu.ar>
2024-06-07 09:09:49 -0300

    Initial commit

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public Text txtScore;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtScore.text = "0";
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        AlimentoScript alimento;
+        alimento = collision.gameObject.GetComponent<AlimentoScript>();
+        score += alimento.valorAlimentario;
+        txtScore.text = score.ToString();
+    }
+}
